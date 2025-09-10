87a4c374873bdc7fc2d36d00f2a17ccb2a9ef534
amarpakhare07 <AmarWaman.Pakhare@cognizant.com>
2025-09-10 17:00:58 +0530

    Refactor Course classes to public and add new models
    Changed the `Course` class to public in `Course.cs`.
    Added `CourseInstructor` and `Progress` classes in their respective files, including necessary properties and navigation properties.
    Updated all files with relevant `using` directives for better code organization.

---
+++ LMS.Domain/Models/CourseInstructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Domain.Models
+{
+    public class CourseInstructor
+    {
+        public int CourseID { get; set; }
+        public int UserID { get; set; }
+        public DateTime AssignedAt { get; set; }
+
+        public Course Course { get; set; } = null!;
+        public User User { get; set; } = null!;
+    }
+}
