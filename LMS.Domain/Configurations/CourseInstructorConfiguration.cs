dde150a54ce5f47c426e9af274aa6ff7b575e36b
2439778 <2439778@LTIN463441>
2025-09-11 18:23:54 +0530

    fluent api for comment,coursecategory,courseInstructor

--- LMS.Domain/Configurations/CourseInstructorConfiguration.cs
+++ LMS.Domain/Configurations/CourseInstructorConfiguration.cs
@@ -7,6 +7,22 @@ namespace Lms.Data.EntityConfigurations {
     public class CourseInstructorConfiguration : IEntityTypeConfiguration<CourseInstructor> {
         public void Configure(EntityTypeBuilder<CourseInstructor> builder) {
             // Configuration goes here
+
+            builder.HasKey(ci => new { ci.CourseID, ci.UserID });
+
+            // Required Timestamp
+            builder.Property(ci => ci.AssignedAt)
+                   .IsRequired();
+
+            // Relationships
+            builder.HasMany(ci => ci.Courses)
+                   .WithOne(c => c.CourseInstructor)
+                   .HasForeignKey(ci => ci.CourseID)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(ci => ci.User)
+                   .WithOne(u => u.CourseInstructor)
+                   .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
