33cf03e8c18d0a1ffa7c29a5932540200a37c9b2
amarpakhare07 <AmarWaman.Pakhare@cognizant.com>
2025-09-11 17:37:30 +0530

    Added Congigurations for Question and Answer

--- LMS.Domain/Configurations/AnswerConfiguration.cs
+++ LMS.Domain/Configurations/AnswerConfiguration.cs
@@ -3,10 +3,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using LMS.Domain.Models;
 
-namespace Lms.Data.EntityConfigurations {
+namespace LMS.Domain.Configurations {
     public class AnswerConfiguration : IEntityTypeConfiguration<Answer> {
         public void Configure(EntityTypeBuilder<Answer> builder) {
             // Configuration goes here
+
+            builder.ToTable("answers");
+            builder.HasKey(x => x.AnswerID);
+            builder.Property(x => x.AnswerID).UseIdentityColumn();
+            builder.Property(x => x.Response).HasColumnType("varchar(max)").IsUnicode(false);
+            builder.Property(x => x.SubmittedAt).HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(x => x.UpdatedAt).HasColumnType("datetime2");
+
+            builder.HasOne(x => x.Question)
+                .WithMany(q => q.Answers)
+                .HasForeignKey(x => x.QuestionID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.User)
+                .WithMany(u => u.Answers)
+                .HasForeignKey(x => x.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(x => new { x.QuestionID, x.UserID, x.SubmittedAt });
+
         }
     }
 }
--- LMS.Domain/Configurations/QuestionConfiguration.cs
+++ LMS.Domain/Configurations/QuestionConfiguration.cs
@@ -3,10 +3,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using LMS.Domain.Models;
 
-namespace Lms.Data.EntityConfigurations {
+namespace LMS.Domain.Configurations {
     public class QuestionConfiguration : IEntityTypeConfiguration<Question> {
         public void Configure(EntityTypeBuilder<Question> builder) {
             // Configuration goes here
+
+            builder.ToTable("questions");
+            builder.HasKey(x => x.QuestionID);
+            builder.Property(x => x.QuestionID).UseIdentityColumn();
+            builder.Property(x => x.QuestionText).IsRequired().HasColumnType("varchar(max)").IsUnicode(false);
+            builder.Property(x => x.QuestionType).HasMaxLength(50).IsUnicode(false);
+            builder.Property(x => x.Options).HasColumnType("varchar(max)").IsUnicode(false);
+            builder.Property(x => x.CorrectAnswer).HasColumnType("varchar(max)").IsUnicode(false);
+            builder.Property(x => x.Marks);
+            builder.Property(x => x.CreatedAt).HasColumnType("datetime2").HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(x => x.UpdatedAt).HasColumnType("datetime2");
+            builder.Property(x => x.IsDeleted).HasDefaultValue(false);
+
+            builder.HasOne(x => x.Quiz)
+                .WithMany(q => q.Questions)
+                .HasForeignKey(x => x.QuizID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasQueryFilter(x => !x.IsDeleted);
+
         }
     }
 }
