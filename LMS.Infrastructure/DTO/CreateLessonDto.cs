a5e29633a2c8f4efaae831d9307fede9a31b0792
Shreyash Murlidhar Nawalkar <2439828@cognizant.com>
2025-09-15 15:02:49 +0530

    Added Repository, Services, Dtos, Controllers for Lesson Module

---
+++ LMS.Infrastructure/DTO/CreateLessonDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Infrastructure.DTO
+{
+    public class CreateLessonDto
+    {
+        [Required]
+        public int CourseID { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        public string? Content { get; set; }
+        public string? VideoURL { get; set; }
+        public int? OrderIndex { get; set; }
+        public string? LessonType { get; set; }
+        public int? EstimatedTime { get; set; }
+
+     
+    }
+}
---
+++ LMS.Infrastructure/DTO/LessonDto.cs
@@ -0,0 +1,32 @@
+﻿using LMS.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Infrastructure.DTO
+{
+    public class LessonDto
+    {
+        public int LessonID { get; set; }
+        [Required]
+        public int CourseID { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+        public string? Content { get; set; }
+        public string? VideoURL { get; set; }
+        public int? OrderIndex { get; set; }
+        public string? LessonType { get; set; }
+        public int? EstimatedTime { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
+
+
+
