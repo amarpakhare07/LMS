92938075950072cc724bdebb8d0c63450ed2d425
2439778 <2439778@LTIN463441>
2025-09-18 11:57:22 +0530

    changed quiz controller and quiz model

--- LMS.Domain/Models/Quiz.cs
+++ LMS.Domain/Models/Quiz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace LMS.Domain.Models
 {
     public class Quiz
     {
+        [Key]
         public int QuizID { get; set; }
         public int CourseID { get; set; }
         public string Title { get; set; } = null!;
